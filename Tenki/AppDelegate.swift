3891bbbe4990448d0b4d98d0367ca2b6ce67305b
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2018-03-10 19:47:34 +0200

    Initial commit

--- Tenki/AppDelegate.swift
+++ Tenki/AppDelegate.swift
@@ -11,16 +11,27 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem.button?.title = "--°"
+        statusItem.action = #selector(AppDelegate.displayPopUp(_:))
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    @objc func displayPopUp(_ sender: AnyObject) {
+        let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil)
+        guard let vc = storyboard.instantiateController(withIdentifier:
+            NSStoryboard.SceneIdentifier(rawValue: "WeatherViewController")) as? WeatherViewController else {
+            return
+        }
+        let popOver = NSPopover()
+        popOver.contentViewController = vc
+        popOver.behavior = .transient
+        popOver.show(relativeTo: (statusItem.button?.bounds)!, of: statusItem.button!, preferredEdge: .maxY)
+    }
 }
 
--- Tenki/WeatherViewController.swift
+++ Tenki/WeatherViewController.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class WeatherViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
