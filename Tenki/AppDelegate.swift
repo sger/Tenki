54a564f62ab3183d21b33c2d8edd801d9f6a669e
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2018-03-10 10:35:49 +0200

    Initial Commit

---
+++ Tenki/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tenki
+//
+//  Created by Spiros Gerokostas on 10/03/2018.
+//  Copyright © 2018 Spiros Gerokostas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tenki/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tenki
+//
+//  Created by Spiros Gerokostas on 10/03/2018.
+//  Copyright © 2018 Spiros Gerokostas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
